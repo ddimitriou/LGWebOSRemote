1eda86c94aa7c4b1e140fe9fa42c2e605dbe320d
Dimitris Dimitriou <dimitriou.d.a@gmail.com>
2018-01-07 17:05:36 +0200

    Packages

--- LGTV/lgtv.py
+++ LGTV/lgtv.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import sys
 import json
 from inspect import getargspec
-from LGTV import LGTVScan, LGTVClient, getCommands
+from . import LGTVScan, LGTVClient, getCommands
 
 
 def usage(error=None):
